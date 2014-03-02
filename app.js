14c31955389b3bf6fda5b5e4266654ea3bc5e6b6
baconstarvation <ant.ucd@gmail.com>
2014-03-02 10:08:21 -0800

    now identical to the gist file at https://gist.github.com/auser/64f2380eb45088d73e39

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
 angular.module('myApp', ['ngRoute'])
 .service('Reddit', function($http) {
-
-	this.getArticles = function(subreddit) {
-			subreddit = subreddit || 'angularjs';
-
-			return $http({
-				method: 'JSONP',
-				url: 'http://www.reddit.com/r/' + subreddit + '.json',
-				params: {
-					'jsonp': 'JSON_CALLBACK' 
-				}
-			}).then(function(resp) {
-				return resp.data.data.children;
-			});
-
-		}
+ 
+  this.getArticles = function(subreddit) {
+    subreddit = subreddit || 'angularjs';
+ 
+    return $http({
+      method: 'JSONP',
+      url: 'http://www.reddit.com/r/' + subreddit + '.json',
+      params: {
+        'jsonp': 'JSON_CALLBACK'
+      }
+    }).then(function(resp) {
+      return resp.data.data.children;
+    });
+ 
+  }
 })
 .config(function($routeProvider) {
-	$routeProvider
-		.when('/:subreddit', {
-		// templateUrl: 'home.html',
-			template: '<h1>Hello {{user.name}} from the home route</h1>',
-			controller: 'HomeController'
-		})
-		.otherwise({
-			redirectTo: '/angularjs'
-		})
+  $routeProvider
+    .when('/:id', {
+      templateUrl: 'home.html',
+      controller: 'HomeController'
+    })
+    .otherwise({
+      redirectTo: '/angularjs'
+    })
 })
-.controller('HomeController', function($scope, Reddit) {
-	$scope.user = { name: 'baconator' };
-	var subreddit = $routeParams.id;
-
-	$scope.getArticles = function(subreddit) {
-		Reddit.getArticles(subreddit)
-		.then(function(articles) {
-				$scope.articles = articles;	
-		});
-	}
-
-	$scope.getArticles();
+.controller('HomeController', function($scope, $routeParams, Reddit) {
+  $scope.user = {name: 'Ari'};
+  var subreddit = $routeParams.id;
+ 
+  Reddit.getArticles(subreddit)
+  .then(function(articles) {
+    $scope.articles = articles;
+  });
 });
